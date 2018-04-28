3c15edd0c40afc0a8d4851f1256198a25d90535f
rhesaav <rhesaav@gmail.com>
2018-04-28 21:33:45 +0700

    + add main2activity

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1052029/farmgo/Intromanager.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1052029/farmgo/Intromanager.java
@@ -1,10 +1,30 @@
 package id.sch.smktelkom_mlg.afinal.xirpl1052029.farmgo;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by ASUS on 4/28/2018.
  */
 
 public class Intromanager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context context;
 
+    public Intromanager(Context context) {
+        this.context = context;
+        pref = context.getSharedPreferences("first", 0);
+        editor = pref.edit();
+    }
+
+    public void setFirst(Boolean isFirst) {
+        editor.putBoolean("check", isFirst);
+        editor.commit();
+    }
+
+    public boolean Check() {
+        return pref.getBoolean("check", true);
+    }
 
 }
