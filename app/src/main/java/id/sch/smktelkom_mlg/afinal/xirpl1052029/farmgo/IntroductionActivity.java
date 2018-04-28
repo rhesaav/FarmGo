24b9739121b9f1d7a842d60acfc9cf94315b550f
rhesaav <rhesaav@gmail.com>
2018-04-28 22:48:56 +0700

    + create viewpager adapter + make some method + call the method

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1052029/farmgo/IntroductionActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1052029/farmgo/IntroductionActivity.java
@@ -2,20 +2,57 @@ package id.sch.smktelkom_mlg.afinal.xirpl1052029.farmgo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class IntroductionActivity extends AppCompatActivity {
 
+    Button next, skip;
     private int[] layouts;
     private ViewPager viewPager;
     private Intromanager intromanager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private TextView[] dots;
+    private LinearLayout dotsLayout;
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+            if (position == layouts.length - 1) {
+                next.setText("Proceed");
+                skip.setVisibility(View.VISIBLE);
+            } else {
+                next.setText("Next");
+                skip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +63,44 @@ public class IntroductionActivity extends AppCompatActivity {
             startActivity(i);
             finish();
         }
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_introduction);
 
+        viewPager = findViewById(R.id.view_pager);
+        dotsLayout = findViewById(R.id.layoutDots);
+        skip = findViewById(R.id.btn_skip);
+        next = findViewById(R.id.btn_next);
         layouts = new int[]{R.layout.activity_screen_1, R.layout.activity_screen_2,
                 R.layout.activity_screen_3, R.layout.activity_screen_4,};
+        addBottomDots(0);
+        changeStatusBarColor();
+        viewPagerAdapter = new ViewPagerAdapter();
+    }
+
+    private void addBottomDots(int position) {
+        dots = new TextView[layouts.length];
+        int[] colorActive = getResources().getIntArray(R.array.dot_active);
+        int[] colorInactive = getResources().getIntArray(R.array.dot_inactive);
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorInactive[position]);
+            dotsLayout.addView(dots[i]);
+        }
+        if (dots.length > 0)
+            dots[position].setTextColor(colorActive[position]);
+    }
+
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 
     public class ViewPagerAdapter extends PagerAdapter {
