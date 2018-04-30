fbabe5ff1086a0676d128a05bf1ea850db3588a0
rhesaav <rhesaav@gmail.com>
2018-04-30 08:36:18 +0700

    + create function to enable the button on optional login

---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1052029/farmgo/OptionalLogin.java
@@ -0,0 +1,29 @@
+package id.sch.smktelkom_mlg.afinal.xirpl1052029.farmgo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by ASUS on 4/30/2018.
+ */
+
+public class OptionalLogin extends AppCompatActivity {
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToDaftar(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_optional_login);
+    }
+}
