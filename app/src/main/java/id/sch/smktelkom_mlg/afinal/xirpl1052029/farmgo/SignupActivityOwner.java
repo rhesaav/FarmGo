78c0969686a49375ce99f9c9acd0f6dbd224fa10
Dimas <alfian_sugara_25rpl@student.smktelkom-mlg.sch.id>
2018-05-02 02:18:03 +0700

    + add login google for owner

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1052029/farmgo/SignupActivityOwner.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1052029/farmgo/SignupActivityOwner.java
@@ -1,6 +1,8 @@
 package id.sch.smktelkom_mlg.afinal.xirpl1052029.farmgo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -8,10 +10,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivityOwner extends AppCompatActivity {
+
+    public static final int RequestSignInCode = 7;
+    public FirebaseAuth firebaseAuth;
+    public GoogleApiClient googleApiClient;
+    DatabaseReference databaseMember;
+    Button SignOutButton;
+    com.google.android.gms.common.SignInButton signInButton;
 
     DatabaseReference databaseOwner;
     private EditText rName, rTelp, rUsername, rPassword, rNamaperusahaan, rAlamat;
@@ -38,6 +59,79 @@ public class SignupActivityOwner extends AppCompatActivity {
             }
         });
 
+        signInButton = findViewById(R.id.signInButtonOwner);
+        signInButton = findViewById(R.id.signInButtonOwner);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(SignupActivityOwner.this)
+                .enableAutoManage(SignupActivityOwner.this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                } /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                UserSignInMethod();
+
+            }
+        });
+
+    }
+
+    public void UserSignInMethod() {
+
+        Intent AuthIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(AuthIntent, RequestSignInCode);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RequestSignInCode) {
+
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (googleSignInResult.isSuccess()) {
+
+                GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
+
+                FirebaseUserAuth(googleSignInAccount);
+            }
+
+        }
+    }
+
+    public void FirebaseUserAuth(GoogleSignInAccount googleSignInAccount) {
+
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+
+        Toast.makeText(SignupActivityOwner.this, " " + authCredential.getProvider(), Toast.LENGTH_LONG).show();
+
+        firebaseAuth.signInWithCredential(authCredential)
+                .addOnCompleteListener(SignupActivityOwner.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> AuthResultTask) {
+
+                        Intent i = new Intent(SignupActivityOwner.this, SignupActivityOwner.class);
+                        startActivity(i);
+                        finish();
+
+                    }
+                });
     }
 
     private void addOwner() {
